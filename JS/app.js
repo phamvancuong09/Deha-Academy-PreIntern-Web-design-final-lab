82fa662521c41134a1b18b18feee4e68de133dfc
phamvancuong09 <cuongpham03012001@gmail.com>
2024-06-27 15:54:28 +0700

    Initial commit

---
+++ JS/app.js
@@ -0,0 +1,8 @@
+document.getElementById('menu-icon').onclick = function() {
+    var nav = document.getElementById('nav');
+    if (nav.style.display === 'block') {
+        nav.style.display = 'none';
+    } else {
+        nav.style.display = 'block';
+    }
+}
